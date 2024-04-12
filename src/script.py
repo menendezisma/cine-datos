f82cae87f8bb425fc485b173fb53b98e7eed16f2
menendezisma <mauriciomenendez01@gmail.com>
2024-04-12 00:34:34 -0600

    reacomodando las columnas del dataframe y limpiando codigo

--- src/script.py
+++ src/script.py
@@ -31,37 +31,34 @@ excel_merged=excel_merged.loc[:,['Title', 'Wk', 'Thu\nAdm 03-Jan', 'Fri\nAdm 04-
                          'Wed\nAdm 09-Jan', 'Week\nAdm']]
 
 #Archivo excel consolidado
-#excel_merged.to_excel('consolidado.xlsx')
+excel_merged.to_excel('consolidado.xlsx')
 
 # Calcular la suma de 'Thu\nAdm 03-Jan' para el día inicial
 initial_day = excel_merged[excel_merged['Wk'] == 1].groupby('Title')['Thu\nAdm 03-Jan'].sum().reset_index()
-print(initial_day)
-print('------------')
+
 # Calcular la suma de 'Weekend\nAdm' para el primer fin de semana
 first_weekend = excel_merged[excel_merged['Wk'] == 1].groupby('Title')['Weekend\nAdm'].sum().reset_index()
-print(first_weekend)
-print('------------')
+
 # Calcular la suma de 'Week\nAdm' para la primera semana
 first_week = excel_merged[excel_merged['Wk'] == 1].groupby('Title')['Week\nAdm'].sum().reset_index()
 #first_week = excel_merged.groupby('Title')['Week\nAdm'].sum().reset_index()
-print(first_week)
-print('------------')
+
 # Calcular la suma de 'Week\nAdm' para la segunda semana
 second_week = excel_merged[excel_merged['Wk'] == 2].groupby('Title')['Week\nAdm'].sum().reset_index()
-print(second_week)
-print('------------')
+
 # Calcular la suma de 'Thu\nAdm 03-Jan', 'Fri\nAdm 04-Jan', 'Sat\nAdm 05-Jan', 'Sun\nAdm 06-Jan', 'Weekend\nAdm', 'Mon\nAdm 07-Jan', 'Tue\nAdm 08-Jan', 'Wed\nAdm 09-Jan', 'Week\nAdm' para el total
 total = excel_merged.groupby('Title')[['Thu\nAdm 03-Jan', 'Fri\nAdm 04-Jan', 'Sat\nAdm 05-Jan', 'Sun\nAdm 06-Jan', 'Weekend\nAdm', 'Mon\nAdm 07-Jan', 'Tue\nAdm 08-Jan', 'Wed\nAdm 09-Jan', 'Week\nAdm']].sum().reset_index()
 total['Total']=total[['Thu\nAdm 03-Jan', 'Fri\nAdm 04-Jan', 'Sat\nAdm 05-Jan', 'Sun\nAdm 06-Jan', 'Weekend\nAdm', 'Mon\nAdm 07-Jan', 'Tue\nAdm 08-Jan', 'Wed\nAdm 09-Jan', 'Week\nAdm']].sum(axis=1)
 total_movie=total[['Title','Total']]
-print(total_movie)
-print('------------')
-# Calcular la suma de 'Thu\nAdm 03-Jan', 'Fri\nAdm 04-Jan', 'Sat\nAdm 05-Jan', 'Sun\nAdm 06-Jan', 'Weekend\nAdm', 'Mon\nAdm 07-Jan', 'Tue\nAdm 08-Jan', 'Wed\nAdm 09-Jan', 'Week\nAdm' para el valor restante
-#remaining = excel_merged[excel_merged['Wk'] >= 3].groupby('Title')[['Thu\nAdm 03-Jan', 'Fri\nAdm 04-Jan', 'Sat\nAdm 05-Jan', 'Sun\nAdm 06-Jan', 'Weekend\nAdm', 'Mon\nAdm 07-Jan', 'Tue\nAdm 08-Jan', 'Wed\nAdm 09-Jan', 'Week\nAdm']].sum().reset_index()
-#remaining['remaing_total']=remaining[['Thu\nAdm 03-Jan', 'Fri\nAdm 04-Jan', 'Sat\nAdm 05-Jan', 'Sun\nAdm 06-Jan', 'Weekend\nAdm', 'Mon\nAdm 07-Jan', 'Tue\nAdm 08-Jan', 'Wed\nAdm 09-Jan', 'Week\nAdm']].sum(axis=1)
-#remainig_movie=remaining[['Title','remaing_total']]
-#print(remainig_movie)
-print('------------')
+
+"""
+Calcular la suma de 'Thu\nAdm 03-Jan', 'Fri\nAdm 04-Jan', 'Sat\nAdm 05-Jan', 'Sun\nAdm 06-Jan', 'Weekend\nAdm', 'Mon\nAdm 07-Jan', 'Tue\nAdm 08-Jan', 'Wed\nAdm 09-Jan', 'Week\nAdm' para el valor restante
+remaining = excel_merged[excel_merged['Wk'] >= 3].groupby('Title')[['Thu\nAdm 03-Jan', 'Fri\nAdm 04-Jan', 'Sat\nAdm 05-Jan', 'Sun\nAdm 06-Jan', 'Weekend\nAdm', 'Mon\nAdm 07-Jan', 'Tue\nAdm 08-Jan', 'Wed\nAdm 09-Jan', 'Week\nAdm']].sum().reset_index()
+remaining['remaing_total']=remaining[['Thu\nAdm 03-Jan', 'Fri\nAdm 04-Jan', 'Sat\nAdm 05-Jan', 'Sun\nAdm 06-Jan', 'Weekend\nAdm', 'Mon\nAdm 07-Jan', 'Tue\nAdm 08-Jan', 'Wed\nAdm 09-Jan', 'Week\nAdm']].sum(axis=1)
+remainig_movie=remaining[['Title','remaing_total']]
+print(remainig_movie)
+"""
+
 # Fusionar todos los resultados en un solo DataFrame
 result = initial_day.merge(first_weekend, on='Title', how='outer').merge(first_week, on='Title', how='outer').merge(second_week, on='Title', how='outer').merge(total_movie, on='Title', how='outer')
 result.columns = ['Title', 'Dia inicial', 'Primer Fin de semana', 'Primera semana', 'Segunda semana', 'Total']
@@ -70,7 +67,7 @@ result['Restante']=result['Total']-(result['Primera semana']+result['Segunda sem
 result['Primera Semana PCT']=(result['Primera semana']/result['Total']*100).round(2).astype(str) + '%'
 result['Segunda semana PCT']=(result['Segunda semana']/result['Total']*100).round(2).astype(str) + '%'
 result['Restante PCT']=(result['Restante']/result['Total']*100).round(2).astype(str) + '%'
-#result=result.reindex(columns=['Title', 'Dia inicial', 'Primer Fin de semana', 'Primera semana', 'Primera Semana PCT', 'Segunda semana', 'Segunda Semana PCT', 'Restante', 'Total'])
+result=result.reindex(columns=['Title', 'Dia inicial', 'Primer Fin de semana', 'Primera semana', 'Primera Semana PCT', 'Segunda semana', 'Segunda semana PCT', 'Restante','Restante PCT','Total'])
 print(result)
-#result.to_excel('resultado4.xlsx')
+result.to_excel('resultado.xlsx')
 #result.to_csv('re.txt', sep=" ", quoting=csv.QUOTE_NONE, escapechar=" ")
